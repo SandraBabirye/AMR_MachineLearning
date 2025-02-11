f27b2647845b09c10858a90ac7482b861f8d98fe
Sandra Babirye <99010085+SandraBabirye@users.noreply.github.com>
2025-02-11 14:38:30 +0300

    Update run_tbprofiler.sh

--- scripts/Bioinformatics_scripts/run_tbprofiler.sh
+++ scripts/Bioinformatics_scripts/run_tbprofiler.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-#Directories to store output files
+# Directories to store output files
+Reads="Fastq_files/"
+SAMPLE_FILE="Sample_list.txt"
 
-mkdir bam vcf results
+# Create necessary directories
+mkdir -p bam vcf results
 
-#To silence the citation option
-
+# To silence the citation option
 parallel --citation --bibtex
 
-cat Samples_list.txt | parallel --bar -j 2 tb-profiler profile -1 $Reads{}_1.fastq.gz -2  Research_container/input/{}_2.fastq.gz -p {} 
+# Run TB-Profiler in parallel
+cat "$SAMPLE_FILE" | parallel --bar -j 2 tb-profiler profile -1 "$Reads"{}_1.fastq.gz -2 "$Reads"input/{}_2.fastq.gz -p {}
