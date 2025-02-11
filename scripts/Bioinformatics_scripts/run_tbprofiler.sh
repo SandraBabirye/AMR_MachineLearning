4ffb4118c54c648851581b04854079edc37419e6
Sandra Babirye <99010085+SandraBabirye@users.noreply.github.com>
2025-02-11 14:36:14 +0300

    Update run_tbprofiler.sh

--- scripts/Bioinformatics_scripts/run_tbprofiler.sh
+++ scripts/Bioinformatics_scripts/run_tbprofiler.sh
@@ -1,1 +1,11 @@
-h
+#!/bin/bash
+
+#Directories to store output files
+
+mkdir bam vcf results
+
+#To silence the citation option
+
+parallel --citation --bibtex
+
+cat Samples_list.txt | parallel --bar -j 2 tb-profiler profile -1 $Reads{}_1.fastq.gz -2  Research_container/input/{}_2.fastq.gz -p {} 
